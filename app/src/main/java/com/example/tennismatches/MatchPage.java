7a809162af54c961ee26d7ccc0b2fb5d632bcc70
Alberto <alberto.ursino@studenti.unipd.it>
2022-09-24 19:47:46 +0200

    feat: implemented the MatchPage

--- app/src/main/java/com/example/tennismatches/MatchPage.java
+++ app/src/main/java/com/example/tennismatches/MatchPage.java
@@ -10,14 +10,25 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.database.AppDatabase;
 import com.example.database.entities.Match;
 import com.example.database.entities.MatchDao;
+import com.example.database.entities.Opponent;
+import com.example.database.entities.OpponentDao;
 import com.google.gson.Gson;
 
+import org.reactivestreams.Subscription;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
 import io.reactivex.CompletableObserver;
+import io.reactivex.FlowableSubscriber;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.functions.Action;
@@ -44,12 +55,25 @@ public class MatchPage extends AppCompatActivity {
         AppDatabase database = Room.databaseBuilder(getApplicationContext(), AppDatabase.class, "tennis-matches-db")
                 .build();
         MatchDao matchDao = database.matchDao();
+        OpponentDao opponentDao = database.opponentDao();
 
         Match match = new Gson().fromJson(getIntent().getStringExtra("matchSelected"), Match.class);
 
-        Button button = findViewById(R.id.delete_match_btn);
+        // Populating fields
+        ((TextView) findViewById(R.id.result_tw)).setText(match.getResult());
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.ITALIAN);
+        String strDate = dateFormat.format(match.getDate());
+        ((TextView) findViewById(R.id.date_tw)).setText(strDate);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        opponentDao.findByMatchId(match.getMatchId())
+                .subscribeOn(Schedulers.from(executorService))
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new getOpponentByMatchId());
+
+        // Delete match button
+        Button deleteMatchBtn = findViewById(R.id.delete_match_btn);
+
+        deleteMatchBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 matchDao.delete(match)
@@ -87,5 +111,28 @@ public class MatchPage extends AppCompatActivity {
         intent.putExtra("lastFragment", "matchesList");
         startActivity(intent);
         finish();
+    }
+
+    private class getOpponentByMatchId implements FlowableSubscriber<Opponent> {
+
+        @Override
+        public void onSubscribe(Subscription s) {
+            s.request(Long.MAX_VALUE);
+        }
+
+        @Override
+        public void onNext(Opponent opp) {
+            ((TextView) findViewById(R.id.opp_tw)).setText(opp.getFirstName() + " " + opp.getLastName());
+        }
+
+        @Override
+        public void onError(Throwable t) {
+
+        }
+
+        @Override
+        public void onComplete() {
+
+        }
     }
 }
